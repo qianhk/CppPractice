f1c8c84795265d9799603a7582bbb6f988f4f519
kaiH <kaiH@baolei7.cn>
2021-02-15 21:20:05 +0800

    test use dll by CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "ModernCpp/modernMain.h"
 #include "UseMathFunctions/useMathMain.h"
 #include "WinApi/usewinapi.h"
+#include "DllFunc/library.h"
 
 int _tmain(int argc, char *argv[]) {
 
@@ -21,6 +22,16 @@ int _tmain(int argc, char *argv[]) {
     mainUseMath(argc, argv);
 
     mainUseWinApi(argc, argv);
+
+    hello();
+
+    PWSTR path;
+    HRESULT result = getRoamingAppDataPath(&path);
+    if (result == S_OK) {
+        wprintf(L"lookKai invoke getKnownFolderPath success: %ls\n", path);
+    } else {
+        wprintf(L"lookKai invoke getKnownFolderPath failed: %lu\n", result);
+    }
 
     return 0;
 }
